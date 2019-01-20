2c1fc1cebdb7c3919d29104258d8804ac2899210
claridiva2000 <claridiva2000@gmail.com>
2019-01-20 16:20:50 -0600

    closure complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const lastName = 'logan';
+function greet() {
+  const firstName = 'Jasmine';
+  return `My name is ${firstName} ${lastName}.`;
+}
+console.log(greet());
 
 // ==== Challenge 2: Create a counter function ====
 
   // Return a function that when invoked increments and returns a counter variable.
+const counter = () => {
+ let count = 0;
+return function(){
+  return ++count;
+}
+
+};
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -15,7 +31,9 @@
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
