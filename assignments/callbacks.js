3ad4690e68b5a8f6baf5d9cff6c33aa03be3a9f0
claridiva2000 <claridiva2000@gmail.com>
2019-01-20 22:33:13 -0600

    callbacks partially complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -4,7 +4,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
  
 
-  //Given this problem: 
+  // Given this problem: 
   
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
@@ -23,12 +23,21 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
 
+getLength(items, function(length){
+  console.log(length);
+});
+
 function last(arr, cb) {
+  return cb(arr[arr.length-1]);
   // last passes the last item of the array into the callback.
 }
+last(items, function(last){
+console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
